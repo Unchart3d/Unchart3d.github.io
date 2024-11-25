721f42697c433d42c9fb6efed2cd82b90df678cc
Unchart3d <126183308+Unchart3d@users.noreply.github.com>
2024-11-24 23:00:27 -0600

    finished website?

--- JS/homepage.js
+++ JS/homepage.js
@@ -33,3 +33,40 @@ const previousProjects = document.querySelector('.previousProjects')
 previousProjects.addEventListener('mouseenter',stopAutoPic);
 
 previousProjects.addEventListener('mouseleave',startAutoPic);
+
+
+let current = 0;
+let auto;
+const pic = document.querySelector('.currentProjectsContainer');
+
+function show(index) {
+    const totalPics = pic.children.length
+    if (index >= totalPics) {
+        current = 0;
+    }
+    if (index < 0) {
+        current = totalPics - 1;
+    }
+    pic.style.transform = `translateX(-${current * 100}%)`;
+}
+
+function next() {
+    current++;
+    show(current);
+}
+
+function startAuto() {
+    auto = setInterval(next, 3000);
+}
+
+function stopAuto() {
+    clearInterval(auto);
+}
+
+startAuto();
+
+const currentProjects = document.querySelector('.currentProjects')
+
+currentProjects.addEventListener('mouseenter',stopAuto);
+
+currentProjects.addEventListener('mouseleave',startAuto);
