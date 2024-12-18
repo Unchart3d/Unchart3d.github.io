2336783d950707e2abcae7a0cfbd0b6beff20611
Ian Nichols <126183308+Unchart3d@users.noreply.github.com>
2024-12-17 22:33:40 -0600

    updates

--- JS/PastProjects.js
+++ JS/PastProjects.js
@@ -1,12 +1,14 @@
-let slideIndex = [1,1,1,1,1,1];
+let slideIndex = [1,1,1,1,1,1,1,1];
 let slideId = ["project-card", "project-card-1", "project-card-4",
-    "project-card-5", "project-card-8", "project-card-10"]
+    "project-card-5", "project-card-8", "project-card-10", "project-card-14", "project-card-15"]
 showSlides(1,0);
 showSlides(1,1);
 showSlides(1,2);
 showSlides(1,3);
 showSlides(1,4);
 showSlides(1,5);
+showSlides(1,6);
+showSlides(1,7);
 
 function plusSlides(n,no) {
     showSlides(slideIndex[no] += n, no);
